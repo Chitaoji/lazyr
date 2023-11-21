ac99113566ba0566312d09ee2eec764466fd48fa
Chitaoji <2360742040@qq.com>
2023-11-22 00:13:09 +0800

    Update core description

--- lazyr/core.py
+++ lazyr/core.py
@@ -1,5 +1,5 @@
 """
-Contains the core of lazyr: register, wakeup, etc.
+Contains the core of lazyr: register(), wakeup(), etc.
 
 NOTE: this module is private. All functions and objects are available in the main
 `lazyr` namespace - use that instead.
