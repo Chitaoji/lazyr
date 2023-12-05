942c6118a6c4b259234d7d49c7e7ccc05b4793a3
Chitaoji <2360742040@qq.com>
2023-12-05 23:24:05 +0800

    Update setup

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
-"""Setup the package."""
+"""
+Setup the package.
+
+To use this file, you must:
+
+```sh
+$ pip install twine
+$ pip install wheel
+```
+"""
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-# Note: To use the 'upload' functionality of this file, you must:
-#   $ pipenv install twine --dev
-
-import io
 import os
+import re
 import sys
+from pathlib import Path
 from shutil import rmtree
+from typing import List, Union
 
 from setuptools import Command, find_packages, setup
 
@@ -24,24 +31,21 @@ REQUIRED = []
 EXTRAS = {}
 
 
-here = os.path.abspath(os.path.dirname(__file__))
+here = Path(__file__).parent
 
 # Import the README and use it as the long-description.
 try:
-    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
-        LONG_DESCRIPTION = "\n" + f.read()
+    LONG_DESCRIPTION = "\n" + (here / "README.md").read_text()
 except FileNotFoundError:
     LONG_DESCRIPTION = DESCRIPTION
+
 
 # Load the package's __version__.py module as a dictionary.
 about = {}
 python_exec = exec
 if not VERSION:
     PROJECT_SLUG = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(
-        os.path.join(here, PROJECT_SLUG, "__version__.py"), encoding="utf-8"
-    ) as f:
-        python_exec(f.read(), about)
+    python_exec((here / PROJECT_SLUG / "__version__.py").read_text(), about)
 else:
     about["__version__"] = VERSION
 
@@ -82,6 +86,141 @@ class UploadCommand(Command):
         os.system("git push --tags")
 
         sys.exit()
+
+
+def rsplit(
+    pattern: Union[str, re.Pattern],
+    string: str,
+    maxsplit: int = 0,
+    flags: Union[int, re.RegexFlag] = 0,
+) -> List[str]:
+    """
+    Split the string by the occurrences of the pattern. Differences to
+    `re.split()` that all groups in the pattern are also returned, each
+    connected with the substring on its right.
+
+    Parameters
+    ----------
+    pattern : Union[str, re.Pattern]
+        Pattern string.
+    string : str
+        String to be splitted.
+    maxsplit : int, optional
+        Max number of splits, if specified to be 0, there will be no
+        more limits, by default 0.
+    flags : Union[int, re.RegexFlag], optional
+        Regex flag, by default 0.
+
+    Returns
+    -------
+    List[str]
+        List of substrings.
+
+    """
+    splits: List[str] = []
+    searched = re.search(pattern, string, flags=flags)
+    left: str = ""
+    while searched:
+        span = searched.span()
+        splits.append(left + string[: span[0]])
+        left = searched.group()
+        string = string[span[1] :]
+        if len(splits) >= maxsplit > 0:
+            break
+        searched = re.search(pattern, string, flags=flags)
+    splits.append(left + string)
+    return splits
+
+
+def __maxsplit(string: str, maximum: int = 1):
+    head, tail = string, ""
+    if len(string) > maximum:
+        if (i := string.rfind(" ", None, 1 + maximum)) > 0 and (
+            l := string[:i]
+        ).strip():
+            head, tail = l, string[1 + i :]
+        elif (j := string.find(" ", 1 + maximum)) > 0:
+            head, tail = string[:j], string[1 + j :]
+    return head.rstrip(), tail.strip()
+
+
+def word_wrap(string: str, maximum: int = 100) -> str:
+    """
+    Takes a string as input and wraps the text into multiple lines,
+    ensuring that each line has a maximum length of characters.
+
+    Parameters
+    ----------
+    string : str
+        The input text that needs to be word-wrapped.
+    maximum : int, optional
+        Specifies the maximum length of each line in the word-wrapped
+        string, by default 100.
+
+    Returns
+    -------
+        A string with the input string wrapped to a maximum line length
+        of 100 characters.
+
+    """
+    if maximum < 1:
+        raise ValueError(f"expected maximum > 0, got {maximum} instead")
+    lines: List[str] = []
+    for x in string.splitlines():
+        while True:
+            l, x = __maxsplit(x, maximum=maximum)
+            lines.append(l)
+            if not x:
+                break
+    return "\n".join(lines)
+
+
+def readme2doc(readme: str) -> str:
+    """
+    Takes a readme string as input and returns a modified version of the
+    readme string without certain sections.
+
+    Parameters
+    ----------
+    readme : str
+        A string containing the content of a README file.
+
+    Returns
+    -------
+    str
+        A modified version of the readme string.
+
+    """
+    doc = ""
+    for i in rsplit("\n## ", readme):
+        head = re.search(" .*\n", i).group()[1:-1]
+        if head not in {"Installation", "Requirements", "History"}:
+            doc += i
+    doc = re.sub("<!--html-->.*<!--/html-->", "", doc, flags=re.DOTALL)
+    return word_wrap(doc) + "\n\n"
+
+
+class ReadmeFormatError(Exception):
+    """Raised when the README has a wrong format."""
+
+
+# Import the __init__.py and change the module docstring.
+try:
+    init_path = here / PROJECT_SLUG / "__init__.py"
+    module_file = init_path.read_text()
+    NEW_DOC = readme2doc(LONG_DESCRIPTION)
+    if "'''" in NEW_DOC and '"""' in NEW_DOC:
+        raise ReadmeFormatError("Both \"\"\" and ''' are found in the README")
+    if '"""' in NEW_DOC:
+        NEW_DOC = f"'''{NEW_DOC}'''"
+    else:
+        NEW_DOC = f'"""{NEW_DOC}"""'
+    module_file = re.sub(
+        "^\"\"\".*\"\"\"|^'''.*'''|^", NEW_DOC, module_file, flags=re.DOTALL
+    )
+    init_path.write_text(module_file)
+except FileNotFoundError:
+    pass
 
 
 setup(
