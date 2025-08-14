32ae420c1cc9cf592e45d19c218f3e82f11d71a3
Chitaoji <2360742040@qq.com>
2025-08-14 21:20:39 +0800

    Remove __init__

--- __init__.py
+++
@@ -1,9 +0,0 @@
-"""Imports from '.src'. This file will be exlcuded from the package."""
-
-from typing import TYPE_CHECKING
-
-from .src import *
-from .src import __all__
-
-if TYPE_CHECKING:
-    from .src import _typing
